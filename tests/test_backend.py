6cf14800ddb76d78a1e8689e3b4065d371278425
Yoni Jacobi <yony@slatescience.com>
2025-09-14 14:45:53 +0300

    fix isort

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,7 +1,8 @@
-import django
-from django.core.cache import InvalidCacheBackendError
 from unittest import TestCase
 from unittest.mock import Mock, patch
+
+import django
+from django.core.cache import InvalidCacheBackendError
 from nose.tools import eq_, raises
 
 from django_elastipymemcache.client import ConfigurationEndpointClient
